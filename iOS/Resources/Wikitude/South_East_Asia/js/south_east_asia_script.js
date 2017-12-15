260e3b29ba2727f7972b7aaa2b6a683cb869b33c
hutch <steviehutch@gmail.com>
2017-12-15 19:45:39 +0000

    added question and answer buttons to South East Asia level

--- iOS/Resources/Wikitude/South_East_Asia/js/south_east_asia_script.js
+++ iOS/Resources/Wikitude/South_East_Asia/js/south_east_asia_script.js
@@ -5,6 +5,10 @@ var World = {
 		this.createOverlays();
 	},
 
+    
+
+
+
 	createOverlays: function createOverlaysFn() {
 		/*
 			First an AR.ClientTracker needs to be created in order to start the recognition engine. It is initialized with a URL specific to the target collection. Optional parameters are passed as object in the last argument. In this case a callback function for the onLoaded trigger is set. Once the tracker is fully loaded the function worldLoaded() is called.
@@ -13,7 +17,8 @@ var World = {
 			Use a specific target name to respond only 
             to a certain target or use a wildcard to respond to any or a certain group of targets.
 		*/
-    
+
+        
 
         this.tracker = new AR.ClientTracker("assets/EdenTrailsMarker_v2.wtc", {
             onLoaded: this.worldLoaded
@@ -23,13 +28,56 @@ var World = {
 			The next step is to create the augmentation. In this example an image resource is created and passed to the AR.ImageDrawable. A drawable is a visual component that can be connected to an IR target (AR.Trackable2DObject) or a geolocated object (AR.GeoObject). The AR.ImageDrawable is initialized by the image and its size. Optional parameters allow for position it relative to the recognized target.
 		*/
 
+        this.imgButton = new AR.ImageResource("assets/roul-roul-correct-answer.png");
+        this.imgButton2 = new AR.ImageResource("assets/gecko-wrong-answer.png");
+
 		// Create overlay for ASIA quiz question
 		var imgOne = new AR.ImageResource("assets/southeast-asia-quiz-question.png");
+        
 		var overlayOne = new AR.ImageDrawable(imgOne, 1, {
-			offsetX: -0.15,
-			offsetY: 0
+			offsetX: 1.55, //-0.15,
+			offsetY: 1.00
 		});
-       
+
+        var pageOneButton = this.createWwwButton1("https://mapmycrush.com/EdenTrails/Islands/congratulations.png", 0.5, {
+            translate: {
+                x: 1.25,
+                y: 0.25
+            },
+            zOrder:1
+        });
+
+        /*
+        var imgTwo = new AR.ImageResource("assets/imageTwo.png");
+        var overlayTwo = new AR.ImageDrawable(imgTwo, 0.5, {
+            translate: {
+                x: 0.12,
+                y: -0.01
+            }
+        });
+
+        */
+
+        var pageTwoButton = this.createWwwButton2("https://mapmycrush.com/EdenTrails/Islands/error.png", 0.5, {
+            translate: {
+                x: 1.85,
+                y: 0.25
+            }//,
+            //zOrder:1
+        });
+
+
+
+        /*
+        var pageOne = new AR.ImageTrackable(this.tracker, "", {
+            drawables: 
+            {
+                cam: [overlayOne, pageOneButton]
+            }
+        });
+        */
+
+
 
 		// Please note that in this case the target name is a wildcard. Wildcards can be used to 
         // respond to any target defined in the target collection. If you want to respond to a certain target 
@@ -83,10 +131,10 @@ var World = {
             viewportHeight: 400,
             backgroundColor: "#00000000",
             translate: {
-                x:0.36,
-                y: 0.5
+                x: 0.0, //x:0.36,
+                y: 0.0
             },
-            horizontalAnchor: AR.CONST.HORIZONTAL_ANCHOR.RIGHT,
+            horizontalAnchor: AR.CONST.HORIZONTAL_ANCHOR.LEFT,
             verticalAnchor: AR.CONST.VERTICAL_ANCHOR.BOTTOM,
             clickThroughEnabled: true,
             allowDocumentLocationChanges: false,
@@ -220,10 +268,11 @@ var World = {
         });
         */
 
-        // RHYS SOUTH EAST ASIA PAINTING MARKER - show Sam's frog....
+        // RHYS SOUTH EAST ASIA PAINTING MARKER - show Sams frog....
         var page1 = new AR.ImageTrackable(this.tracker, "Rhys_Asia_Marker", {
             drawables: {
-            cam: [overlayOne, treeFrogWidget, samsFrogWidget]
+            cam: [overlayOne, treeFrogWidget, samsFrogWidget, pageOneButton, pageTwoButton]
+            //cam: [overlayOne, pageOneButton, pageTwoButton]
             },
         });
 
@@ -242,6 +291,20 @@ var World = {
         }
         });*/
 	},
+
+    createWwwButton1: function createWwwButtonFn(url, size, options) {
+            options.onClick = function() {
+            AR.context.openInBrowser(url);
+        };
+        return new AR.ImageDrawable(this.imgButton, size, options);
+    },
+
+    createWwwButton2: function createWwwButtonFn(url, size, options) {
+            options.onClick = function() {
+            AR.context.openInBrowser(url);
+        };
+        return new AR.ImageDrawable(this.imgButton2, size, options);
+    },
 
 	worldLoaded: function worldLoadedFn() {
 		var cssDivInstructions = " style='display: table-cell;vertical-align: middle; text-align: right; width: 50%; padding-right: 15px;'";       
